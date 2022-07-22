e32643310bcbdc8637a9904ef1ef6637a89c77bb
Seshathri S <seshathris@Seshathris-MacBook-Air.local>
2022-07-22 16:40:10 +0530

    removed unused modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
+import { Dialect } from 'sequelize/types';
+
+/*
+
+*/
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'password',
+      database: 'dochost',
+      models: [],
+      autoLoadModels: true,
+      synchronize: true,
+      sync: {
+        force: true,
+      },
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+const { Pool, Client } = require('pg');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(3001);
 }
+var pg = require('pg');
+//or native libpq bindings
+//var pg = require('pg').native
+/*
+var conString =
+  'postgres://rjcdxluh:bbhFJZkCZ89AEyh2mFaqpac9RH-LmQDO@satao.db.elephantsql.com/rjcdxluh'; //Can be found in the Details page
+var client = new pg.Client(conString);
+client.connect(function (err) {
+  if (err) {
+    return console.error('could not connect to postgres', err);
+  }
+  client.query('SELECT NOW() AS "theTime"', function (err, result) {
+    if (err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].theTime);
+    // >> output: 2018-08-23T14:02:57.117Z
+    client.end();
+  });
+});
+
+*/
 bootstrap();
